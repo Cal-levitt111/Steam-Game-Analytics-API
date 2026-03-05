3eb85374703c82ce6f15cbdb7da91b4390831ee2
Cal Levitt <cal.levitt111@gmail.com>
2026-03-05 15:36:11 +0000

    test(config): set allowed hosts for TestClient defaults

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,4 @@ def pytest_sessionstart(session):
     os.environ.setdefault('SECRET_KEY', 'test-secret-key')
     os.environ.setdefault('ACCESS_TOKEN_EXPIRE_MINUTES', '1440')
     os.environ.setdefault('ENVIRONMENT', 'test')
+    os.environ.setdefault('ALLOWED_HOSTS', 'testserver,localhost,127.0.0.1')
